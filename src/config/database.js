04abd2e2c931a8eb860f11e47816b20ffc36a407
Facundo Cristaldo <facundocristaldo146@gmail.com>
2025-09-15 10:07:42 -0300

    configuraccion de la conexion a la base de datos establecida

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+  try {
+    await mongoose.connect("mongodb://127.0.0.1:27017/mongoose_db");
+    // await mongoose.connection.dropDatabase();
+    console.log("Conectado a la base de datos");
+  } catch (error) {
+    console.log("No se pudo conectar a la base de datos", error);
+  }
+};
